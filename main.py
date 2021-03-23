61d74055ac11cd8aaa390f77ab086f69c57dd532
Jeremiah Coholich <jcoholich@gatech.edu>
2021-03-23 00:12:56 -0400

    asdf

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import yaml
 
+import argparse
+
+
+
 with open(r'config/defaults.yaml') as f:
     default_params = yaml.full_load(f)
 print(default_params)
+
+test_dict = {'asdf':39393, 'gringotts': 43}
+
+parser = argparse.ArgumentParser()
+# parser.add_argument("--verbose", help="increase output verbosity",default=False,
+#                     action="store_true")
+args = parser.parse_args()
+
+vars(args).update(test_dict)
+
+breakpoint()
 
