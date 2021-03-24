4063c91fe55298aaf0ad047bd8b59b3c5e43edeb
Jeremiah Coholich <jcoholich@gatech.edu>
2021-03-23 21:18:49 -0400

    asdf

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import argparse
+from warnings import warn as warn_
+
+import torch
 import yaml
 
-import argparse
+from pytorch_a2c_ppo_acktr_gail.main import main as ppo_main
+
+def warn(text):
+    warn_('\033[93m' + text + '\033[0m')
+
+
+def get_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", 
+                        help="specify name of yaml config file. If none is given, use default.yaml",
+                        type=str,
+                        default="default")
+    args = parser.parse_args()
+
+    with open(r'config/defaults.yaml') as f:
+        default_params = yaml.full_load(f)
+
+    vars(args).update(default_params)
+    if args.cuda and not torch.cuda.is_available():
+        warn('No GPU found, running on CPU.')
+        args.cuda = False
+
+    return args
 
 
 
-with open(r'config/defaults.yaml') as f:
-    default_params = yaml.full_load(f)
-print(default_params)
+def main():
+    args = get_params()
+    ppo_main(args)
 
-test_dict = {'asdf':39393, 'gringotts': 43}
 
-parser = argparse.ArgumentParser()
-# parser.add_argument("--verbose", help="increase output verbosity",default=False,
-#                     action="store_true")
-args = parser.parse_args()
-
-vars(args).update(test_dict)
-
-breakpoint()
+if __name__ == '__main__':
+    main()
 
