dbc6b321982b1ed4dab4f77e704ee271e67a2d53
Jeremiah Coholich <jcoholich@gatech.edu>
2021-04-13 02:11:06 -0400

    fixed bug where observations were always zero

--- aliengo_env/modular_obs_and_act.py
+++ aliengo_env/modular_obs_and_act.py
@@ -18,13 +18,15 @@ class Observation():
 
         assert isinstance(parts, list)
         self.parts = parts
-        self.handles = {'joint_torques': quadruped.applied_torques, 
-                'joint_positions': quadruped.joint_positions, 
-                'joint_velocities': quadruped.joint_velocities, 
-                'base_angular_velocity': quadruped.base_avel,
-                'base_roll': quadruped.base_orientation[0, None],
-                'base_pitch': quadruped.base_orientation[1, None],
-                'base_yaw': quadruped.base_orientation[2, None]}
+        self.quadruped = quadruped
+        self.handles = {'joint_torques': self.get_applied_torques, 
+                'joint_positions': self.get_joint_positions, 
+                'joint_velocities': self.get_joint_velocities, 
+                'base_angular_velocity': self.get_base_angular_velocity,
+                'base_roll': self.get_base_roll,
+                'base_pitch': self.get_base_pitch,
+                'base_yaw': self.get_base_yaw,
+                'trajectory_generator_phases': self.get_tg_phases}
         self.lengths = {'joint_torques': 12, 
                 'joint_positions': 12, 
                 'joint_velocities': 12, 
@@ -32,6 +34,7 @@ class Observation():
                 'base_roll': 1,
                 'base_pitch': 1,
                 'base_yaw': 1}
+                # 'trajectory_generator_phases': 8}
         assert all(part in self.handles.keys() for part in parts)
         self.parts.sort() # to insure that simply passing the parts in a different order doesn't specify a different env
 
@@ -39,11 +42,36 @@ class Observation():
         for part in parts: self.obs_len += self.lengths[part] 
         self.observation_lb = -np.ones(self.obs_len)
         self.observation_ub = np.ones(self.obs_len)
+    
+
+    def get_applied_torques(self):
+        return self.quadruped.applied_torques
+
+    def get_joint_positions(self):
+        return self.quadruped.joint_positions
+
+    def get_joint_velocities(self):
+        return self.quadruped.joint_velocities
+
+    def get_base_angular_velocity(self):
+        return self.quadruped.base_avel
+
+    def get_base_roll(self):
+        return self.quadruped.base_orientation[0, None]
+
+    def get_base_pitch(self):
+        return self.quadruped.base_orientation[1, None]
+
+    def get_base_yaw(self):
+        return self.quadruped.base_orientation[2, None]
+
+    def get_tg_phases(self):
+        return np.concatenate((np.sin(self.quadruped.phases), np.cos(self.quadruped.phases)))
 
     def __call__(self):
         """Gets observation and returns it. """
         # obs = np.zeros(self.obs_len)
-        obs = np.concatenate([self.handles[part] for part in self.parts])
+        obs = np.concatenate([self.handles[part]() for part in self.parts])
         return obs
 
 
