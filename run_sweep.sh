b8e643499a45afd07efe796fd48ea54af372958a
Jeremiah Coholich <jcoholich@gatech.edu>
2021-05-28 16:17:33 -0400

    ready for footstep ars sweep

---
+++ run_sweep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+
+wait
