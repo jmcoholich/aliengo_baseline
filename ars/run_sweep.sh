40509a4cf67209ac1d5e16d52e0af2270a62336d
Jeremiah Coholich <jcoholich@gatech.edu>
2021-05-27 22:10:13 -0400

    update sweep agent

--- ars/run_sweep.sh
+++ ars/run_sweep.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-wandb agent jcoholich/aliengo_baseline-ars/nwv4lclx &
-
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
+wandb agent jcoholich/aliengo_baseline-ars/y6bfeqti &
 
 wait
