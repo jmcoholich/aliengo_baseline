c92e853a66107fa9aecef84e4f099f5874acc79c
Jeremiah Coholich <jcoholich@gatech.edu>
2021-05-28 16:44:59 -0400

    update sweep agent

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
-wandb agent jcoholich/footstep_ars_sweep/2qfd931f &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
+wandb agent jcoholich/footstep_ars_sweep/w63v28n4 &
 
 wait
